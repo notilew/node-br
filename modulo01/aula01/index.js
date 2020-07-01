ee9fa7262403ce3399a47851310fd4dc1bce81d1
Wellington Felix <wellington.felix@avancoinfo.com.br>
2020-07-01 15:07:44 -0300

    refatorando aula01.

--- modulo01/aula01/index.js
+++ modulo01/aula01/index.js
@@ -1,29 +1,22 @@
+/**
+ * Ciclo de Vida de Funções Javascript - Com Callbacks
+ */
+
 const obterUsuario = (callback) => {
     setTimeout(() => {
-        return callback(null, {
-            id: 1,
-            nome: 'wellington',
-            nascimento: new Date()
-        });
+        return callback(null, { id: 1, nome: 'wellington', nascimento: new Date('1989-06-16T07:45:00') });
     }, 1000);
 };
 
 const obterTelefone = (id, callback) => {
     setTimeout(() => {
-        return callback(null, {
-            fixo: '31 3322-8563',
-            movel: '31 9 8685-7816'
-        });
+        return callback(null, { fixo: '31 3322-8563', movel: '31 9 8685-7816' });
     }, 2000);
 };
 
 const obterEndereco = (id, callback) => {
     setTimeout(() => {
-        return callback(null, {
-            rua: 'ibirapuera',
-            numero: 480,
-            bairro: 'itaipu'
-        });
+        return callback(null, { rua: 'ibirapuera', numero: 480, bairro: 'itaipu' });
     }, 2000);
 };
 
@@ -36,8 +29,8 @@ obterUsuario((erro, usuario) => {
         obterEndereco(usuario.id, (erro, endereco) => {
             if (erro) throw new Error(erro);
 
-            console.log('usuário: ', endereco);
-            console.log('telefone: ', endereco);
+            console.log('usuário: ', usuario);
+            console.log('telefone: ', telefone);
             console.log('endereço: ', endereco);
         });
     });
